52cba3b4f0219fd67def9b654e8f02c74f62abf8
Hugo <hugomouchonpro@gmail.com>
2022-12-03 16:14:08 +0100

    Fin du developpement des sections header, Accueil, Références, Carousel et Footer

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 
 // Partie Javascript du carousel d'images
 //---------------------------------------------------------
+
 const slidesContainer = document.getElementById("slides-container");
 const slide = document.querySelector(".slide");
 const prevButton = document.getElementById("slide-arrow-prev");
@@ -15,5 +16,22 @@ prevButton.addEventListener("click", () => {
   const slideWidth = slide.clientWidth;
   slidesContainer.scrollLeft -= slideWidth;
 });
+
 //------------------------------------------------
+
+// Condition pour rendre visible le header lors du scroll de l'utilisateur
+//---------------------------------------------------------
+
+const hiddenHeader = document.querySelector ("header");
+
+window.addEventListener("scroll", () => {
+  console.log(window.scrollY);
+  if (window.scrollY > 200) {
+    hiddenHeader.style.display = '';
+  } else {
+    hiddenHeader.style.display = 'none';
+  }
+});
+
+//--------------------------------------------------
 
